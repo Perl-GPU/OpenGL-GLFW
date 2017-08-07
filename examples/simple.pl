b6143d686f1c24380560c416a69e78fce2aefacb
Chris Marshall <devel.chm.01@gmail.com>
2017-08-07 12:46:17 -0400

    Tidy up simple.pl code

--- examples/simple.pl
+++ examples/simple.pl
@@ -25,7 +25,7 @@
 
 #========================================================================
 # OpenGL routines used:
-# 
+#
 #   glAttachShader
 #   glBindBuffer
 #   glClear
@@ -37,15 +37,15 @@
 #   glLinkProgram
 #   glUseProgram
 #   glViewport
-#   
+#
 #   glGetAttribLocation_c
 #   glGetUniformLocation_c
 #   glUniformMatrix4fv_c
 #   glVertexAttribPointer_c
-#   
+#
 #   glBufferData_p
 #   glGenBuffers_p
-#   glGenBuffers_p 
+#   glGenBuffers_p
 #   glShaderSource_p
 #========================================================================
 
@@ -56,9 +56,7 @@ use OpenGL::Modern::Helpers qw(glGenBuffers_p glBufferData_p);
 # #include "linmath.h"
 
 #                   x     y    r    g    b
-my @vertices = (  -0.6, -0.4, 1.0, 0.0, 0.0,
-                   0.6, -0.4, 0.0, 1.0, 0.0,
-                   0.0,  0.6, 0.0, 0.0, 1.0 );
+my @vertices = ( -0.6, -0.4, 1.0, 0.0, 0.0, 0.6, -0.4, 0.0, 1.0, 0.0, 0.0, 0.6, 0.0, 0.0, 1.0 );
 
 my @vertex_shader_text = (
     "uniform mat4 MVP;\n",
@@ -68,17 +66,11 @@ my @vertex_shader_text = (
     "void main()\n",
     "{\n",
     "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n",
-    "    color = vCol;\n",
-    "}\n"
+    "    color = vCol;\n", "}\n"
 );
 
-my @fragment_shader_text = (
-    "varying vec3 color;\n",
-    "void main()\n",
-    "{\n",
-    "    gl_FragColor = vec4(color, 1.0);\n",
-    "}\n",
-);
+my @fragment_shader_text =
+  ( "varying vec3 color;\n", "void main()\n", "{\n", "    gl_FragColor = vec4(color, 1.0);\n", "}\n", );
 
 # my $drop_callback = sub {
 #     my ($window, @paths) = @_;
@@ -86,24 +78,24 @@ my @fragment_shader_text = (
 # };
 
 my $error_callback = sub {
-    my ($error, $description) = @_;
+    my ( $error, $description ) = @_;
     printf STDERR "Error #%d from perl: %s\n", $error, $description;
 };
 
 my $key_callback = sub {
-    my ($window, $key, $scancode, $action, $mods) = @_;
-    if ($key == GLFW_KEY_ESCAPE && $action == GLFW_PRESS) {
-        glfwSetWindowShouldClose($window, GLFW_TRUE);
+    my ( $window, $key, $scancode, $action, $mods ) = @_;
+    if ( $key == GLFW_KEY_ESCAPE && $action == GLFW_PRESS ) {
+        glfwSetWindowShouldClose( $window, GLFW_TRUE );
     }
 };
 
 # int main(void) {
 #
-my ($window);
-my ($vertex_buffer, $vertex_shader, $fragment_shader, $program);
-my ($mvp_location, $vpos_location, $vcol_location);
+my ( $window );
+my ( $vertex_buffer, $vertex_shader, $fragment_shader, $program );
+my ( $mvp_location, $vpos_location, $vcol_location );
 
-glfwSetErrorCallback($error_callback);
+glfwSetErrorCallback( $error_callback );
 
 die "glfwInit failed, $!\n" if !glfwInit();
 
@@ -113,13 +105,13 @@ die "glfwInit failed, $!\n" if !glfwInit();
 # TODO: implement NULL pointer (is 0 or undef enough?)
 # $window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
 #
-$window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-unless (defined $window) {
+$window = glfwCreateWindow( 640, 480, "Simple example", NULL, NULL );
+unless ( defined $window ) {
     glfwTerminate();
     die "glfwCreateWindow failed, $!\n";
 }
 
-glfwSetKeyCallback($window, $key_callback);
+glfwSetKeyCallback( $window, $key_callback );
 
 # TODO: It looks like you cant Drag-and-Drop between windows Explorer
 # and cygwin X11 applications.  Need to test for linux and for win32
@@ -127,79 +119,82 @@ glfwSetKeyCallback($window, $key_callback);
 #
 # glfwSetDropCallback($window, $drop_callback);
 
-glfwMakeContextCurrent($window);
+glfwMakeContextCurrent( $window );
 
 die "glewInit failed, $!\n" if GLEW_OK != glewInit();
 
-glfwSwapInterval(1);
+glfwSwapInterval( 1 );
 
 #-----------------------------------------------------------
 #  NOTE: OpenGL error checks have been omitted for brevity
 #-----------------------------------------------------------
 
-my $vertex_buffer = glGenBuffers_p(1);  # TODO
-glBindBuffer(GL_ARRAY_BUFFER, $vertex_buffer);
+my $vertex_buffer = glGenBuffers_p( 1 );    # TODO
+glBindBuffer( GL_ARRAY_BUFFER, $vertex_buffer );
+
 # glBufferData_p(GL_ARRAY_BUFFER, sizeof(@vertices), @vertices, GL_STATIC_DRAW);  # TODO
-glBufferData_p(GL_ARRAY_BUFFER, 4*scalar(@vertices), @vertices, GL_STATIC_DRAW);  # TODO
+glBufferData_p( GL_ARRAY_BUFFER, 4 * scalar( @vertices ), @vertices, GL_STATIC_DRAW );    # TODO
 
-my $vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-glShaderSource_p($vertex_shader, @vertex_shader_text);  # TODO
-glCompileShader($vertex_shader);
+my $vertex_shader = glCreateShader( GL_VERTEX_SHADER );
+glShaderSource_p( $vertex_shader, @vertex_shader_text );                                  # TODO
+glCompileShader( $vertex_shader );
 
-my $fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-glShaderSource_p($fragment_shader, @fragment_shader_text);  # TODO
-glCompileShader($fragment_shader);
+my $fragment_shader = glCreateShader( GL_FRAGMENT_SHADER );
+glShaderSource_p( $fragment_shader, @fragment_shader_text );                              # TODO
+glCompileShader( $fragment_shader );
 
 my $program = glCreateProgram();
-glAttachShader($program, $vertex_shader);
-glAttachShader($program, $fragment_shader);
-glLinkProgram($program);
+glAttachShader( $program, $vertex_shader );
+glAttachShader( $program, $fragment_shader );
+glLinkProgram( $program );
 
-my $mvp_location  = glGetUniformLocation_c($program, "MVP");  # TODO change name to _p or no-suffix
-my $vpos_location = glGetAttribLocation_c($program, "vPos");  # TODO change name to _p or no-suffix
-my $vcol_location = glGetAttribLocation_c($program, "vCol");  # TODO change name to _p or no-suffix
+my $mvp_location = glGetUniformLocation_c( $program, "MVP" );    # TODO change name to _p or no-suffix
+my $vpos_location = glGetAttribLocation_c( $program, "vPos" );   # TODO change name to _p or no-suffix
+my $vcol_location = glGetAttribLocation_c( $program, "vCol" );   # TODO change name to _p or no-suffix
 
-glEnableVertexAttribArray($vpos_location);
+glEnableVertexAttribArray( $vpos_location );
+
 #------------------------------------------------------------ stride, offset
-glVertexAttribPointer_c($vpos_location, 2, GL_FLOAT, GL_FALSE, 4 * 5, 0     );
-glEnableVertexAttribArray($vcol_location);
-#------------------------------------------------------------ stride, offset
-glVertexAttribPointer_c($vcol_location, 3, GL_FLOAT, GL_FALSE, 4 * 5, 4 * 2 );
+glVertexAttribPointer_c( $vpos_location, 2, GL_FLOAT, GL_FALSE, 4 * 5, 0 );
+glEnableVertexAttribArray( $vcol_location );
 
-while (!glfwWindowShouldClose($window))
-{
-    my ($width, $height);
-    my ($angle, $c, $s, $cor, $sor);
+#------------------------------------------------------------ stride, offset
+glVertexAttribPointer_c( $vcol_location, 3, GL_FLOAT, GL_FALSE, 4 * 5, 4 * 2 );
+
+while ( !glfwWindowShouldClose( $window ) ) {
+    my ( $width, $height );
+    my ( $angle, $c, $s, $cor, $sor );
+
     # mat4x4 $m, $p, $mvp;  # TODO
 
-    my ($width, $height) = glfwGetFramebufferSize($window);
+    my ( $width, $height ) = glfwGetFramebufferSize( $window );
     my $ratio = $width / $height;
 
-    glViewport(0, 0, $width, $height);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport( 0, 0, $width, $height );
+    glClear( GL_COLOR_BUFFER_BIT );
 
     # $m = mat4x4_identity();  # TODO
     # $m = mat4x4_rotate_Z($m, glfwGetTime());  # TODO
     $angle = glfwGetTime();
-    $c = cos($angle);
-    $cor = $c/$ratio;
-    $s = sin($angle);
-    $sor = $s/$ratio;
+    $c     = cos( $angle );
+    $cor   = $c / $ratio;
+    $s     = sin( $angle );
+    $sor   = $s / $ratio;
 
     # $p = mat4x4_ortho(-$ratio, $ratio, -1.0, 1.0, 1.0, -1.0);  # TODO
     # hardwired rotation and orthographic projection MVP matrix
     my $mvp = pack 'f[16]', $cor, $s, 0, 0, -$sor, $c, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1;
     my $pointer_to_mvp_data = unpack 'Q', pack 'P', $mvp;
 
-    glUseProgram($program);
-    glUniformMatrix4fv_c($mvp_location, 1, GL_FALSE, $pointer_to_mvp_data);  # TODO
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glUseProgram( $program );
+    glUniformMatrix4fv_c( $mvp_location, 1, GL_FALSE, $pointer_to_mvp_data );    # TODO
+    glDrawArrays( GL_TRIANGLES, 0, 3 );
 
-    glfwSwapBuffers($window);
+    glfwSwapBuffers( $window );
     glfwPollEvents();
 }
 
-glfwDestroyWindow($window);
+glfwDestroyWindow( $window );
 
 glfwTerminate();
 #
